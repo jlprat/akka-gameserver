88caba98ff137a46a2af00b808092dacbfe1306d
Josep Prat <jlprat@gmail.com>
2015-03-15 15:01:54 +0100

    hand as a player's member. Improve testability

--- src/main/scala/com/github/jlprat/gameserver/actors/Player.scala
+++ src/main/scala/com/github/jlprat/gameserver/actors/Player.scala
@@ -14,6 +14,8 @@ class Player (val id: Int, val tableActor: ActorRef, val client: ActorRef) exten
   import com.github.jlprat.gameserver.model._
   import context._
 
+  var playersHand = Hand()
+
   /**
    * state where the player waits for getting the cards dealt
    * @return
@@ -22,7 +24,8 @@ class Player (val id: Int, val tableActor: ActorRef, val client: ActorRef) exten
     case TakenCards(hand, playerId) if playerId == id =>
       log.info(s"I receive a hand $hand")
       client ! Out.ReceiveCard(hand, playerId)
-      become(inactivePlayer(hand), discardOld = true)
+      playersHand = hand
+      become(inactivePlayer(), discardOld = true)
     case TakenCards(hand, playerId) =>
       log.info(s"Player $playerId receives ${hand.size} cards")
       client ! Out.ReceiveCardOpponent(hand.size, playerId)
@@ -33,27 +36,26 @@ class Player (val id: Int, val tableActor: ActorRef, val client: ActorRef) exten
 
   /**
    * State where the player is not in turn and waits for getting active
-   * @param hand the hand of the player
    * @return
    */
-  def inactivePlayer(hand: Hand): Receive = {
+  def inactivePlayer(): Receive = {
     case TakenCards(hand, playerId) if playerId != id =>
       log.info(s"Player $playerId receives ${hand.size} cards")
       client ! Out.ReceiveCardOpponent(hand.size, playerId)
     case NextTurn(playerId) if playerId == id =>
       log.info(s"I receive a Next Turn message for me")
       client ! Out.PlayerInTurn(playerId)
-      become(activePlayer(hand), discardOld = true)
+      become(activePlayer(), discardOld = true)
     case NextTurn(playerId) =>
       log.info(s"I receive a Next Turn message for $playerId")
       client ! Out.PlayerInTurn(playerId)
     case In.Leave => tableActor ! Leave(id)
-    case In.AnnounceLastCard if hand.size == 1 =>
+    case In.AnnounceLastCard if playersHand.size == 1 =>
       tableActor ! AnnounceLastCard(id)
-    case In.AnnounceLastCard if hand.size > 1 =>
+    case In.AnnounceLastCard if playersHand.size > 1 =>
       tableActor ! TakeCard(id)
       client ! Out.WrongAction
-      become(playerMadeAction(hand), discardOld = true)
+      become(playerMadeAction(), discardOld = true)
     case _ : In.Incoming =>
       log.error("Not your turn")
       client ! Out.NotInTurn
@@ -63,30 +65,30 @@ class Player (val id: Int, val tableActor: ActorRef, val client: ActorRef) exten
   /**
    * State where the player is in turn and can perform actions
    * TODO install a timer for the turn timeout
-   * @param hand the hand of the player
    * @return
    */
-  def activePlayer(hand: Hand): Receive = {
-    case msg @ In.PlayCardRequest(card) if hand.exists( _ == card) =>
+  def activePlayer(): Receive = {
+    case msg @ In.PlayCardRequest(card) if playersHand.exists( _ == card) =>
       log.info(s"Player tries to play a card we have $msg")
-      val (cardOption, newHand) = hand.play(card)
+      val (cardOption, newHand) = playersHand.play(card)
       cardOption.foreach(playedCard => tableActor ! PlayCard(playedCard, id))
-      become(playerMadeAction(newHand), discardOld = true)
+      playersHand = newHand
+      become(playerMadeAction(), discardOld = true)
     case In.PlayCardRequest(_) =>
       log.error("Client tries to play a card we don't have!")
       client ! Out.WrongAction
     case In.TakeCardsRequest =>
       tableActor ! TakeCard(id)
-      become(playerMadeAction(hand), discardOld = true)
+      become(playerMadeAction(), discardOld = true)
     case In.Leave =>
       tableActor ! Leave(id)
       become(leftPlayer, discardOld = true)
-    case In.AnnounceLastCard if hand.size == 1 =>
+    case In.AnnounceLastCard if playersHand.size == 1 =>
       tableActor ! AnnounceLastCard(id)
-    case In.AnnounceLastCard if hand.size > 1 =>
+    case In.AnnounceLastCard if playersHand.size > 1 =>
       tableActor ! TakeCard(id)
       client ! Out.WrongAction
-      become(playerMadeAction(hand), discardOld = true)
+      become(playerMadeAction(), discardOld = true)
     case NextTurn(playerId) if playerId != id =>
       //TODO this should be a clear corner case once the timers are installed
       log.error("I am out of sync, or somebody impersonated me!")
@@ -95,32 +97,34 @@ class Player (val id: Int, val tableActor: ActorRef, val client: ActorRef) exten
 
   /**
    * State where the player took an action and is waiting for a proper answer
-   * @param hand the hand of the player
    * @return
    */
-  def playerMadeAction(hand: Hand): Receive = {
+  def playerMadeAction(): Receive = {
     case ChangeSuitRequest =>
       client ! Out.SelectSuitRequest
-      become(changeSuit(hand), discardOld = true)
+      become(changeSuit(), discardOld = true)
     case PlayedCard(card, playerId) =>
       client ! Out.PlayedCardSuccessfully(card, playerId)
     case PlayedCardIllegal(card, playerId) =>
       client ! Out.PlayedCardIrregularly(card, playerId)
-      become(activePlayer(card :: hand), discardOld = true)
-    case TakenCards(receivedCards, playerId) if playerId == id => become(playerWaitingForNextTurn(hand:::receivedCards), discardOld = true)
+      playersHand = card :: playersHand
+      become(activePlayer(), discardOld = true)
+    case TakenCards(receivedCards, playerId) if playerId == id =>
+      playersHand = playersHand ::: receivedCards
+      become(playerWaitingForNextTurn(), discardOld = true)
     case message => log.debug(s"received $message")
   }
 
-  def playerWaitingForNextTurn(hand: Hand): Receive = {
-    case NextTurn(playerId) if playerId == id => become(activePlayer(hand), discardOld = true)
-    case NextTurn => become(inactivePlayer(hand), discardOld = true)
+  def playerWaitingForNextTurn(): Receive = {
+    case NextTurn(playerId) if playerId == id => become(activePlayer(), discardOld = true)
+    case NextTurn => become(inactivePlayer(), discardOld = true)
     case message => log.debug(s"received $message")
   }
 
-  def changeSuit(hand: Hand): Receive = {
+  def changeSuit(): Receive = {
     case In.SelectSuitRequest(suit) =>
       tableActor ! ChangeSuit(suit, id)
-      become(playerWaitingForNextTurn(hand), discardOld = true)
+      become(playerWaitingForNextTurn(), discardOld = true)
     case message => log.debug(s"received $message")
   }
 
--- src/test/scala/com/github/jlprat/gameserver/actor/PlayerActorSpec.scala
+++ src/test/scala/com/github/jlprat/gameserver/actor/PlayerActorSpec.scala
@@ -2,7 +2,7 @@ package com.github.jlprat.gameserver.actor
 
 import java.util.concurrent.TimeUnit
 
-import akka.actor.{ActorRef, Props, ActorSystem}
+import akka.actor.{Props, ActorSystem}
 import akka.testkit._
 import com.github.jlprat.gameserver.actors.Player
 import com.github.jlprat.gameserver.model.{Card, Hand}
@@ -25,11 +25,11 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
   val clientActorProbe = TestProbe()
   val playerHand = Hand(List.tabulate(5)(elem => Card(elem, elem, "blue")))
 
-  def giveMeAPlayerActor(id: Int = 1) : TestActorRef[Nothing] = {
+  def giveMeAPlayerActor(id: Int = 1) : TestActorRef[Player] = {
     TestActorRef(Props(classOf[Player], id, tableActorProbe.ref, clientActorProbe.ref))
   }
 
-  def giveMeAPlayerInTurn(id: Int = 1) : TestActorRef[Nothing] = {
+  def giveMeAPlayerInTurn(id: Int = 1) : TestActorRef[Player] = {
     val playerActor = giveMeAPlayerActor(id)
     playerActor ! TakenCards(playerHand, playerId = id)
     clientActorProbe.expectMsg(Out.ReceiveCard(playerHand, playerId = id))
@@ -45,10 +45,12 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
         val message = NextTurn(2)
         playerActor ! message
         EventFilter.error(message = s"Unknown message $message", occurrences = 1)
+        assert(playerActor.underlyingActor.playersHand === Hand())
       }
       "send a wrong message to the client if the message comes from the them" in {
         playerActor ! In.PlayCardRequest(Card(1,1,"blue"))
         clientActorProbe.expectMsg(Out.WrongAction)
+        assert(playerActor.underlyingActor.playersHand === Hand())
       }
     }
     "receives a TakenCards message" when {
@@ -60,6 +62,7 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
           playerActor ! message
           clientActorProbe.expectMsg(Out.ReceiveCardOpponent(numberCards = 1, playerId = 2))
           EventFilter.info(message = s"Player ${message.playerId} receives ${message.hand.size} cards", occurrences = 1)
+          assert(playerActor.underlyingActor.playersHand === Hand())
         }
       }
       "is for same player" must {
@@ -67,11 +70,13 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
           val message = TakenCards(Hand(red1), playerId = 1)
           playerActor ! message
           clientActorProbe.expectMsg(Out.ReceiveCard(Hand(red1), playerId = 1))
+          assert(playerActor.underlyingActor.playersHand === Hand(red1))
         }
         "receive other TakenCards for other players" in {
           val message = TakenCards(Hand(Card(2,2,"red")), playerId = 2)
           playerActor ! message
           clientActorProbe.expectMsg(Out.ReceiveCardOpponent(numberCards = 1, playerId = 2))
+          assert(playerActor.underlyingActor.playersHand === Hand(red1))
         }
         "ignore Incoming requests" in {
           val message = In.PlayCardRequest(card = red1)
@@ -89,6 +94,7 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
   "After dealt, a not in turn player" can {
     val playerActor = giveMeAPlayerActor(id = 2)
     playerActor ! TakenCards(playerHand, playerId = 2)
+    assert(playerActor.underlyingActor.playersHand === playerHand)
     clientActorProbe.expectMsg(Out.ReceiveCard(playerHand, playerId = 2))
     "receive Next turn messages for other players, several times" in {
       playerActor ! NextTurn(playerId = 1)
@@ -102,6 +108,7 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
         "client get a notification" in {
           playerActor ! In.PlayCardRequest(cardInHand)
           clientActorProbe.expectMsg(Out.NotInTurn)
+          assert(playerActor.underlyingActor.playersHand === playerHand)
         }
         "table get no message" in {
           tableActorProbe.expectNoMsg(FiniteDuration(100, TimeUnit.MILLISECONDS))
@@ -111,7 +118,8 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
   }
   "A player is in turn" when {
     "receives NextTurn message for thyself" in {
-      giveMeAPlayerInTurn(2)
+      val playerActor = giveMeAPlayerInTurn(2)
+      assert(playerActor.underlyingActor.playersHand === playerHand)
     }
     "After this," can {
       "either play a card" when {
@@ -119,28 +127,47 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
         "not in hand, client must be notified" in {
           playerActor ! In.PlayCardRequest(Card(2, 2, "red"))
           clientActorProbe.expectMsg(Out.WrongAction)
+          assert(playerActor.underlyingActor.playersHand === playerHand)
         }
         "in hand, table must get this request" in {
-          playerActor ! In.PlayCardRequest(Card(0, 0, "blue"))
-          tableActorProbe.expectMsg(PlayCard(Card(0, 0, "blue"), playerId = 2))
+          val cardInHand: Card = Card(0, 0, "blue")
+          playerActor ! In.PlayCardRequest(cardInHand)
+          assert(playerActor.underlyingActor.playersHand === playerHand.play(cardInHand)._2)
+          tableActorProbe.expectMsg(PlayCard(cardInHand, playerId = 2))
         }
       }
       "or ask for more cards" in {
         val playerActor = giveMeAPlayerInTurn(2)
         playerActor ! In.TakeCardsRequest
         tableActorProbe.expectMsg(TakeCard(playerId = 2))
+        assert(playerActor.underlyingActor.playersHand === playerHand)
       }
     }
   }
 
   "Once a player requested to play a card" when {
+    val cardPlayed = Card(1, 1, "blue")
+    val handAfterPlay: Hand = playerHand.play(cardPlayed)._2
     "correctly played" must {
       val playerActor = giveMeAPlayerInTurn(3)
-      playerActor ! In.PlayCardRequest(Card(1, 1, "blue"))
-      tableActorProbe.expectMsg(PlayCard(Card(1, 1, "blue"), playerId = 3))
+      playerActor ! In.PlayCardRequest(cardPlayed)
+      tableActorProbe.expectMsg(PlayCard(cardPlayed, playerId = 3))
+      assert(playerActor.underlyingActor.playersHand === handAfterPlay)
       "receive confirmation that the card was played" in {
         playerActor ! PlayedCard(Card(1, 1, "blue"), playerId = 3)
-        clientActorProbe.expectMsg(Out.PlayedCardSuccessfully(Card(1, 1, "blue"), playerId = 3))
+        clientActorProbe.expectMsg(Out.PlayedCardSuccessfully(cardPlayed, playerId = 3))
+        assert(playerActor.underlyingActor.playersHand === handAfterPlay)
+      }
+    }
+    "incorrectly played" must {
+      val playerActor = giveMeAPlayerInTurn(3)
+      playerActor ! In.PlayCardRequest(cardPlayed)
+      tableActorProbe.expectMsg(PlayCard(cardPlayed, playerId = 3))
+      assert(playerActor.underlyingActor.playersHand === handAfterPlay)
+      "receive confirmation of wrong move" in {
+        playerActor ! PlayedCardIllegal(Card(1, 1, "blue"), playerId = 3)
+        clientActorProbe.expectMsg(Out.PlayedCardIrregularly(cardPlayed, playerId = 3))
+        assert(playerActor.underlyingActor.playersHand.sort === playerHand.sort)
       }
     }
   }
